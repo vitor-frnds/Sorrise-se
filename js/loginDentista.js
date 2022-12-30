3bad4fe4bb0ebdf105e08f0abd6d7c0e774217a2
Vitor Fernandes Reis <vitor.fernandes@estudante.ufjf.br>
2022-12-30 16:52:43 -0300

    Correções de aparência e funcionalidade de ver a senha no login Paciente

--- js/loginDentista.js
+++ js/loginDentista.js
@@ -1,5 +1,4 @@
 let btn = document.querySelector('.fa-eye');
-let btnConfirm = document.querySelector('#verConfirmSenha');
 
 btn.addEventListener('click', ()=>{
     let inputSenha = document.querySelector('#senha')
@@ -13,14 +12,3 @@ btn.addEventListener('click', ()=>{
 
 });
 
-btnConfirm.addEventListener('click', ()=>{
-    let inputConfirmSenha = document.querySelector('#confirmSenha')
-
-    if (inputConfirmSenha.getAttribute('type') == 'password'){
-        inputConfirmSenha.setAttribute('type', 'text')
-    }
-    else {
-        inputConfirmSenha.setAttribute('type', 'password')
-    }
-
-});
---
+++ js/loginPaciente.js
@@ -0,0 +1,13 @@
+let btn = document.querySelector('.fa-eye');
+
+btn.addEventListener('click', ()=>{
+    let inputSenha = document.querySelector('#senha')
+
+    if (inputSenha.getAttribute('type') == 'password'){
+        inputSenha.setAttribute('type', 'text')
+    }
+    else {
+        inputSenha.setAttribute('type', 'password')
+    }
+
+});
